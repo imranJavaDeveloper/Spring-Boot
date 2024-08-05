384c80497778369bdb4c58cbc5e3005a2e2a4ef8
Md Imran Alam <mdimranalam2348@gmail.com>
2024-08-05 23:39:26 +0530

    remove some file

--- CRUD_Example/src/main/java/com/example/CRUD_Example/BookRepositroy.java
+++ CRUD_Example/src/main/java/com/example/CRUD_Example/BookRepositroy.java
@@ -1,8 +1,6 @@
 package com.example.CRUD_Example;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 @Repository
 public interface BookRepositroy extends JpaRepository<Book,Long> {
 }
